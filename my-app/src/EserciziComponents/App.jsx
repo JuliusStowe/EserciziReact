9a90f9ecf54dd1cf4dcdc4f7a47e57e3cbbb58af
Matteo <matteodurso45@gmail.com>
2024-02-14 10:43:32 +0100

    eserciziComponents

---
+++ my-app/src/EserciziComponents/App.jsx
@@ -0,0 +1,15 @@
+// Esercizio 1-4
+import { Hello } from "./ese1";
+import { Message } from "./ese1";
+export function App(){
+return (
+    <div>
+        <Hello/>
+        <Hello/>
+        <Hello/>
+        <Message/>
+    </div>
+)
+}
+
+
---
+++ my-app/src/EserciziComponents/ese1.jsx
@@ -0,0 +1,6 @@
+export function Hello(){
+    return <h1>Hello World!</h1>
+}
+export function Message() {
+	return <p>What a beautiful day!</p>
+}
